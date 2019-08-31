57b46599472b0774b143d68e075d8cdadc72653e
Maksim <familinov@gmail.com>
2019-08-31 22:24:48 +0500

    после созвона, проблема с зелеными полосами у заголовка на странице Каталог продукции(1), в блоке Дополнительные товары необходимо прижать текст с объемом и ценой к нижней границы

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,18 @@
-var button = document.querySelector(".header__toogle");
+var button = document.querySelector(".menu-button__container");
 var nav = document.querySelector(".nav");
-var icon = document.querySelector(".header__icon");
+var icon = document.querySelector(".menu-button");
 
 nav.classList.remove ("nav--show");
+icon.classList.remove("menu-button--close");
 
 button.addEventListener("click", function(evt) {
   nav.classList.toggle("nav--show");
+  if(icon.classList.contains("menu-button--open")) {
+    icon.classList.remove("menu-button--open")
+    icon.classList.add("menu-button--close")
+  }
+  else {
+    icon.classList.remove("menu-button--close")
+    icon.classList.add("menu-button--open")
+  }
 });
